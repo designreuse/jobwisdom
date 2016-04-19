c1f043f206f181c77aad5a9e64adc616f7279c7f
gaoguofan_isus <772846384@qq.com>
2016-04-19 15:58:32 +0800

    qiniu

--- zefun/src/test/java/com/zefun/test/QiniuTestUpload.java
+++ zefun/src/test/java/com/zefun/test/QiniuTestUpload.java
@@ -1,13 +1,14 @@
 package com.zefun.test;
 
-import java.io.File;
-
-import com.qiniu.common.QiniuException;
-import com.qiniu.http.Response;
 import com.qiniu.storage.BucketManager;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 
+/**
+ * 
+* @author 高国藩
+* @date 2016年4月19日 下午2:21:39
+ */
 public class QiniuTestUpload {
 
     /** 七牛应用id */
@@ -20,19 +21,19 @@ public class QiniuTestUpload {
     public static final String DO_MAIN = "http://7xss26.com2.z0.glb.qiniucdn.com";
     
     /**七牛授权对象*/
-    private final static Auth auth = Auth.create(QINIU_AK, QINIU_SK);
+    private static final Auth auth = Auth.create(QINIU_AK, QINIU_SK);
     
     /**七牛上传管理者*/
-    private final static UploadManager uploadManager = new UploadManager();
+    private static final UploadManager uploadManager = new UploadManager();
     
     /**七牛空间管理者*/
     private final BucketManager bucketManager = new BucketManager(auth);
     
-    public static void main(String[] args) throws QiniuException {
-        for (int i = 0; i < 100000; i++) {
-            Response response = uploadManager.put("D:\\20120525022541628.jpg", "jobwisdom/profile/"+i+".jpg", auth.uploadToken(QINIU_SCOPE));
-            String key = response.jsonToMap().get("key").toString();
-            System.out.println(key);
-        }
-    }
+//    public static void main(String[] args) throws QiniuException {
+//        for (int i = 0; i < 100000; i++) {
+//            Response response = uploadManager.put("D:\\20120525022541628.jpg", "jobwisdom/profile/"+i+".jpg", auth.uploadToken(QINIU_SCOPE));
+//            String key = response.jsonToMap().get("key").toString();
+//            System.out.println(key);
+//        }
+//    }
 }
