f2bebca6af5bbdc594cf88298a7f0f95a1136751
gaoguofan_isus <772846384@qq.com>
2016-06-29 18:41:18 +0800

    spring mobile

--- zefun/src/main/java/com/zefun/web/entity/MemberSubAccount.java
+++ zefun/src/main/java/com/zefun/web/entity/MemberSubAccount.java
@@ -7,142 +7,146 @@ import java.math.BigDecimal;
  * @date 2016年03月12日 PM 14:03:53
  */
 public class MemberSubAccount {
-	/** 子账户标识 */
-	private Integer subAccountId;
+    /** 子账户标识 */
+    private Integer subAccountId;
 
-	/** 账户标识 */
-	private Integer accountId;
+    /** 账户标识 */
+    private Integer accountId;
 
-	/** 级别标识 */
-	private Integer levelId;
+    /** 级别标识 */
+    private Integer levelId;
 
-	/** 储值总额 */
-	private BigDecimal totalAmount;
+    /** 储值总额 */
+    private BigDecimal totalAmount;
 
-	/** 赠送总额 */
-	private BigDecimal totalPresentAmount;
+    /** 赠送总额 */
+    private BigDecimal totalPresentAmount;
 
-	/** 储值余额 */
-	private BigDecimal balanceAmount;
+    /** 储值余额 */
+    private BigDecimal balanceAmount;
 
-	/** 使用金额 */
-	private BigDecimal useAmount;
+    /** 使用金额 */
+    private BigDecimal useAmount;
 
-	/** 创建时间 */
-	private String createTime;
+    /** 创建时间 */
+    private String createTime;
 
-	/** 修改时间 */
-	private String updateTime;
+    /** 修改时间 */
+    private String updateTime;
 
-	/** 最后操作人标识 */
-	private Integer lastOperatorId;
+    /** 最后操作人标识 */
+    private Integer lastOperatorId;
 
-	/** 是否删除(1删除，0正常)*/
-	private Integer isDeleted;
-	
-
+    /** 是否删除(1删除，0正常)*/
+    private Integer isDeleted;
 
     /** @param subAccountId	子账户标识 */
-	public void setSubAccountId(Integer subAccountId){
-		this.subAccountId = subAccountId;
-	}
+    public void setSubAccountId(Integer subAccountId) {
+        this.subAccountId = subAccountId;
+    }
 
-	/** @return	子账户标识 */
-	public Integer getSubAccountId(){
-		return subAccountId;
-	}
+    /** @return	子账户标识 */
+    public Integer getSubAccountId() {
+        return subAccountId;
+    }
 
-	/** @param accountId	账户标识 */
-	public void setAccountId(Integer accountId){
-		this.accountId = accountId;
-	}
+    /** @param accountId	账户标识 */
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
 
-	/** @return	账户标识 */
-	public Integer getAccountId(){
-		return accountId;
-	}
+    /** @return	账户标识 */
+    public Integer getAccountId() {
+        return accountId;
+    }
 
-	/** @param levelId	级别标识 */
-	public void setLevelId(Integer levelId){
-		this.levelId = levelId;
-	}
+    /** @param levelId	级别标识 */
+    public void setLevelId(Integer levelId) {
+        this.levelId = levelId;
+    }
 
-	/** @return	级别标识 */
-	public Integer getLevelId(){
-		return levelId;
-	}
+    /** @return	级别标识 */
+    public Integer getLevelId() {
+        return levelId;
+    }
 
-	/** @param totalAmount	储值总额 */
-	public void setTotalAmount(BigDecimal totalAmount){
-		this.totalAmount = totalAmount;
-	}
+    /** @param totalAmount	储值总额 */
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 
-	/** @return	储值总额 */
-	public BigDecimal getTotalAmount(){
-		return totalAmount;
-	}
+    /** @return	储值总额 */
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
 
-	/** @param totalPresentAmount	赠送总额 */
-	public void setTotalPresentAmount(BigDecimal totalPresentAmount){
-		this.totalPresentAmount = totalPresentAmount;
-	}
+    /** @param totalPresentAmount	赠送总额 */
+    public void setTotalPresentAmount(BigDecimal totalPresentAmount) {
+        this.totalPresentAmount = totalPresentAmount;
+    }
 
-	/** @return	赠送总额 */
-	public BigDecimal getTotalPresentAmount(){
-		return totalPresentAmount;
-	}
+    /** @return	赠送总额 */
+    public BigDecimal getTotalPresentAmount() {
+        return totalPresentAmount;
+    }
 
-	/** @param balanceAmount	储值余额 */
-	public void setBalanceAmount(BigDecimal balanceAmount){
-		this.balanceAmount = balanceAmount;
-	}
+    /** @param balanceAmount	储值余额 */
+    public void setBalanceAmount(BigDecimal balanceAmount) {
+        this.balanceAmount = balanceAmount;
+    }
 
-	/** @return	储值余额 */
-	public BigDecimal getBalanceAmount(){
-		return balanceAmount;
-	}
+    /** @return	储值余额 */
+    public BigDecimal getBalanceAmount() {
+        return balanceAmount;
+    }
 
-	/** @param useAmount	使用金额 */
-	public void setUseAmount(BigDecimal useAmount){
-		this.useAmount = useAmount;
-	}
+    /** @param useAmount	使用金额 */
+    public void setUseAmount(BigDecimal useAmount) {
+        this.useAmount = useAmount;
+    }
 
-	/** @return	使用金额 */
-	public BigDecimal getUseAmount(){
-		return useAmount;
-	}
+    /** @return	使用金额 */
+    public BigDecimal getUseAmount() {
+        return useAmount;
+    }
 
-	/** @param createTime	创建时间 */
-	public void setCreateTime(String createTime){
-		this.createTime = createTime;
-	}
+    /** @param createTime	创建时间 */
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
 
-	/** @return	创建时间 */
-	public String getCreateTime(){
-		return createTime;
-	}
+    /** @return	创建时间 */
+    public String getCreateTime() {
+        return createTime;
+    }
 
-	/** @param updateTime	修改时间 */
-	public void setUpdateTime(String updateTime){
-		this.updateTime = updateTime;
-	}
+    /** @param updateTime	修改时间 */
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
 
-	/** @return	修改时间 */
-	public String getUpdateTime(){
-		return updateTime;
-	}
+    /** @return	修改时间 */
+    public String getUpdateTime() {
+        return updateTime;
+    }
 
-	/** @param lastOperatorId	最后操作人标识 */
-	public void setLastOperatorId(Integer lastOperatorId){
-		this.lastOperatorId = lastOperatorId;
-	}
+    /** @param lastOperatorId	最后操作人标识 */
+    public void setLastOperatorId(Integer lastOperatorId) {
+        this.lastOperatorId = lastOperatorId;
+    }
 
-	/** @return	最后操作人标识 */
-	public Integer getLastOperatorId(){
-		return lastOperatorId;
-	}
-	
-   public Integer getIsDeleted() {
+    /** @return	最后操作人标识 */
+    public Integer getLastOperatorId() {
+        return lastOperatorId;
+    }
+     
+    /**
+     * 获取是否删除
+    * @author 高国藩
+    * @date 2016年6月29日 下午6:39:48
+    * @return  isDeleted
+     */
+    public Integer getIsDeleted() {
         return isDeleted;
     }
 
