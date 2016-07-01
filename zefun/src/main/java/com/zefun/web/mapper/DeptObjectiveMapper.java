fb3fadd95b3a892519352c83b373882ff54ca865
Dnsmac <375311552@qq.com>
2016-07-01 18:23:20 +0800

    lf

--- zefun/src/main/java/com/zefun/web/mapper/DeptObjectiveMapper.java
+++ zefun/src/main/java/com/zefun/web/mapper/DeptObjectiveMapper.java
@@ -2,28 +2,16 @@ package com.zefun.web.mapper;
 
 import com.zefun.web.entity.DeptObjective;
 
-/**
- * 关于部门
-* @author chendb
-* @date 2015年9月8日 上午10:12:59
- */
 public interface DeptObjectiveMapper {
-    /**
-     * 新增功能
-    * @author chendb
-    * @date 2015年9月8日 上午10:13:41
-    * @param record 参数
-    * @return int
-     */
+    int deleteByPrimaryKey(Integer objectiveId);
+
     int insert(DeptObjective record);
 
-    /**
-     * 修改功能
-    * @author chendb
-    * @date 2015年9月8日 上午10:13:41
-    * @param record 参数
-    * @return int
-     */
+    int insertSelective(DeptObjective record);
+
+    DeptObjective selectByPrimaryKey(Integer objectiveId);
+
     int updateByPrimaryKeySelective(DeptObjective record);
-    
+
+    int updateByPrimaryKey(DeptObjective record);
 }
